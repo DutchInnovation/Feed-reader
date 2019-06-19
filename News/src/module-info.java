ff444b3a01ef63969b5ec77dca5cfeb312397751
R1ch4rd <32466820+R1ch4rd420@users.noreply.github.com>
2019-06-19 21:46:24 +0200

    Nieuwe Functies Toegevoegd
    Models uitgewerkt, controllers aangesloten en veel bugs gefixt

---
+++ News/src/module-info.java
@@ -0,0 +1,10 @@
+module News {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires gson;
+    requires java.xml;
+    requires retrofit2;
+    requires okhttp3;
+
+    opens sample;
+}
---
+++ News/src/sample/api/ApiClient.java
@@ -0,0 +1,22 @@
+package sample.api;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+public class ApiClient {
+
+    public static final String BASE_URL = "https://newsapi.org/v2";
+    public static Retrofit retrofit;
+
+    public static Retrofit getApiClient() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).client(getUnsafeOKHttpClient().build()).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+
+        return retrofit;
+    }
+
+    public static OkHttpClient.Builder getUnsafeOKHttpClient() {
+        return  null;
+    }
+}
